cb0a1eef6e8b9f27a61c9b23bc7fdbce8a63c039
solomo3512 <34825001+solomo3512@users.noreply.github.com>
2023-04-03 13:50:28 +0800

    Update dsp.go
    add function body

--- dsp/dsp.go
+++ dsp/dsp.go
@@ -19,13 +19,13 @@ package dsp
 // Mixes together two audio frames containing 160 samples. Uses saturation
 // adding so you don't hear clipping if ppl talk loud. Uses 128-bit SIMD
 // instructions so we can add eight numbers at the same time.
-func L16MixSat160(dst, src *int16)
+func L16MixSat160(dst, src *int16){return}
 
 // Compresses a PCM audio sample into a G.711 μ-Law sample. The BSR instruction
 // is what makes this code fast.
 //
 // TODO(jart): How do I make assembly use proper types?
-func LinearToUlaw(linear int64) (ulaw int64)
+func LinearToUlaw(linear int64) (ulaw int64){return}
 
 // Turns a μ-Law byte back into an audio sample.
-func UlawToLinear(ulaw int64) (linear int64)
+func UlawToLinear(ulaw int64) (linear int64){return}
